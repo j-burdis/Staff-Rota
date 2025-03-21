a4b0c17b78f0d68d9e29f47a5739a8485d5ca193
Jonathan Burdis <jonnyburdis@hotmail.com>
2025-03-21 17:37:25 +0000

    add employees and hours to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create default work hours for each day of the week
+[
+  { day_of_week: 'sunday', hours: 5.5 },
+  { day_of_week: 'monday', hours: 7.0 },
+  { day_of_week: 'tuesday', hours: 7.0 },
+  { day_of_week: 'wednesday', hours: 7.0 },
+  { day_of_week: 'thursday', hours: 7.0 },
+  { day_of_week: 'friday', hours: 7.0 },
+  { day_of_week: 'saturday', hours: 7.0 }
+].each do |work_hour_data|
+  WorkHour.find_or_create_by!(day_of_week: work_hour_data[:day_of_week]) do |wh|
+    wh.hours = work_hour_data[:hours]
+  end
+end
+
+# Add some sample employees if needed
+if Rails.env.development? && Employee.count == 0
+  [
+    { name: 'Su' },
+    { name: 'Michelle' },
+    { name: 'Iryna' },
+    { name: 'Nikki' },
+    { name: 'Leona' },
+    { name: 'Mal' },
+    { name: 'Tom' }
+  ].each do |employee_data|
+    Employee.create!(employee_data)
+  end
+end
